28b309628a634449ff27a0798244fd510468f18a
oliviadugger <oliviacdugger@gmail.com>
2018-07-13 18:53:01 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -2,7 +2,7 @@
 class GardenGnome
   attr_reader :name, :age, :gluten_allergy, :personality
   attr_writer :name, :age, :gluten_allergy
-  def initialize(personality)
-    @personality = "evil"
+  def initialize(personality = "evil")
+    @personality = personality
   end
 end
